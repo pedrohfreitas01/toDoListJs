0ee4dcabb6e8304e1fd1bb6da718e5814882568b
pedrohfc <pedrofreitas_ph@hotmail.com>
2024-06-17 22:38:29 -0300

    localStoregeAdd, TaskDoneDelete

--- script.js
+++ script.js
@@ -5,41 +5,60 @@ const fullTaskList = document.querySelector('.list-task')
 let itemListTask = []
 
 function addNewTask() {
-    itemListTask.push(inputTask.value)
+    itemListTask.push({
+        task: inputTask.value,
+        done: false
+    })
     showTask()
-    
     inputTask.value = ''
-    
-    console.log(itemListTask);
-   
 }
 
 function showTask() {
     let newLi = ''
 
-    itemListTask.forEach(task => {
+    itemListTask.forEach((item, index) => {
 
         newLi = newLi + `
-            <li class="task">
-                <img src="assets/checked.png" alt="check" onclick="checkTask()">
-                <p>${task}</p>
-                <img src="assets/trash.png" alt="lixo" onclick="deleteTask()">
+            <li class="task ${item.done && "checked"} ">
+                <img src="assets/checked.png" alt="check" onclick="checkTask(${index})">
+                <p>${item.task}</p>
+                <img src="assets/trash.png" alt="lixo" onclick="deleteTask(${index})">
             </li>
             `
     });
-
     fullTaskList.innerHTML = newLi
 
+    localStorage.setItem('list', JSON.stringify(itemListTask))   
 }
 
-function checkTask(){
-    console.log('well done');
+function checkTask(index){
+    itemListTask[index].done = !itemListTask[index].done
+    console.log('feito');
+
+    showTask()
 }
 
-function deleteTask(){
-    console.log('item deletado');
+function deleteTask(index){
+    itemListTask.splice(index, 1);
+    showTask()  //refaz a LI  
 }
 
+function realoadTask() {
+    const localStorageTasks = localStorage.getItem('list')
+
+    if (localStorageTasks) {
+        itemListTask = JSON.parse(localStorageTasks)
+    }
+
+    showTask()
+
+}
 
 btnAddTask.addEventListener('click', addNewTask)
+inputTask.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        addNewTask()
+    }
+})
 
+realoadTask()
